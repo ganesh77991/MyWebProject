b9cbb845df243f8abcada1ae5c9eb24738c48d3e
Ganesh padala <ganeshpadala@gmail.com>
2018-12-26 23:30:14 +0530

    this is adding log4j to our Project

---
+++ src/main/java/TestLog.java
@@ -0,0 +1,15 @@
+import org.apache.log4j.Logger;
+
+public class TestLog {
+
+	private static final Logger log=Logger.getLogger(TestLog.class);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		log.debug("ganesh Padala");
+		log.info("mounika puligilla");
+		log.warn("annapurna chinnapaka");
+
+	}
+
+}
