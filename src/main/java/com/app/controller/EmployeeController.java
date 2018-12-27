ee4ac5a1d6237ea750036d905e99c03bba6735f7
Ganesh padala <ganeshpadala@gmail.com>
2018-12-27 18:29:49 +0530

    this open

--- src/main/java/com/app/controller/EmployeeController.java
+++ src/main/java/com/app/controller/EmployeeController.java
@@ -1,9 +1,20 @@
 package com.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.app.service.EmployeeService;
 
+@Controller
+@RequestMapping("employee")
 public class EmployeeController {
 	
 	EmployeeService service;
+	
+	@RequestMapping("testing")
+	public String test() {
+		
+		return "registerEmp";
+	}
 
 }
